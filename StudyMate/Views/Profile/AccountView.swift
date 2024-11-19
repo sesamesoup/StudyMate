32d8da950a5ea2160d7ae3816c22463513f36a9d
Maddie Adair <98988491+maddieadair@users.noreply.github.com>
2024-11-19 15:34:34 -0600

    Design changes from last time

---
+++ StudyMate/Views/Profile/AccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountView.swift
+//  StudyMate
+//
+//  Created by Maddie Adair on 10/24/24.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountView()
+}
